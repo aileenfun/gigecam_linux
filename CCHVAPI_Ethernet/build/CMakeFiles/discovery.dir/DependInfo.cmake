b88591732a4adc69ce8cc01d259fa97e5f21b7de
aileenfun <alanliu0618@gmail.com>
2018-10-23 18:57:13 +0800

    fix receive bug

--- CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/DependInfo.cmake
+++ CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/DependInfo.cmake
@@ -4,22 +4,22 @@ SET(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 SET(CMAKE_DEPENDS_CHECK_CXX
-  "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/CCHVAPI.cpp" "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/CCHVAPI.cpp.o"
-  "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/DeviceGVCP.cpp" "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/DeviceGVCP.cpp.o"
-  "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/GigEDataCapture.cpp" "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/GigEDataCapture.cpp.o"
-  "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/GigEDataProcess.cpp" "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/GigEDataProcess.cpp.o"
-  "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/Inc/MV_CrossPlatform/Linux_Thread.cpp" "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/Inc/MV_CrossPlatform/Linux_Thread.cpp.o"
-  "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/Inc/MV_CrossPlatform/MV_Log.cpp" "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/Inc/MV_CrossPlatform/MV_Log.cpp.o"
-  "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/Inc/MV_CrossPlatform/MV_Memory.cpp" "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/Inc/MV_CrossPlatform/MV_Memory.cpp.o"
-  "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/Inc/MV_CrossPlatform/MV_Thread.cpp" "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/Inc/MV_CrossPlatform/MV_Thread.cpp.o"
-  "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/Inc/MV_CrossPlatform/MV_Tools.cpp" "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/Inc/MV_CrossPlatform/MV_Tools.cpp.o"
-  "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/Inc/MV_CrossPlatform/Windows_Thread.cpp" "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/Inc/MV_CrossPlatform/Windows_Thread.cpp.o"
-  "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/Inc/Socket/Socket.cpp" "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/Inc/Socket/Socket.cpp.o"
-  "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/TrigImgPack.cpp" "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/TrigImgPack.cpp.o"
-  "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/camProp.cpp" "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/camProp.cpp.o"
-  "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/clientProp.cpp" "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/clientProp.cpp.o"
-  "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/main.cpp" "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/main.cpp.o"
-  "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/wqueue.cpp" "/home/hyf/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/wqueue.cpp.o"
+  "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/CCHVAPI.cpp" "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/CCHVAPI.cpp.o"
+  "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/DeviceGVCP.cpp" "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/DeviceGVCP.cpp.o"
+  "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/GigEDataCapture.cpp" "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/GigEDataCapture.cpp.o"
+  "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/GigEDataProcess.cpp" "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/GigEDataProcess.cpp.o"
+  "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/Inc/MV_CrossPlatform/Linux_Thread.cpp" "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/Inc/MV_CrossPlatform/Linux_Thread.cpp.o"
+  "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/Inc/MV_CrossPlatform/MV_Log.cpp" "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/Inc/MV_CrossPlatform/MV_Log.cpp.o"
+  "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/Inc/MV_CrossPlatform/MV_Memory.cpp" "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/Inc/MV_CrossPlatform/MV_Memory.cpp.o"
+  "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/Inc/MV_CrossPlatform/MV_Thread.cpp" "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/Inc/MV_CrossPlatform/MV_Thread.cpp.o"
+  "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/Inc/MV_CrossPlatform/MV_Tools.cpp" "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/Inc/MV_CrossPlatform/MV_Tools.cpp.o"
+  "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/Inc/MV_CrossPlatform/Windows_Thread.cpp" "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/Inc/MV_CrossPlatform/Windows_Thread.cpp.o"
+  "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/Inc/Socket/Socket.cpp" "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/Inc/Socket/Socket.cpp.o"
+  "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/TrigImgPack.cpp" "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/TrigImgPack.cpp.o"
+  "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/camProp.cpp" "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/camProp.cpp.o"
+  "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/clientProp.cpp" "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/clientProp.cpp.o"
+  "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/main.cpp" "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/main.cpp.o"
+  "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/wqueue.cpp" "/home/nxb/gigecam_linux/CCHVAPI_Ethernet/build/CMakeFiles/discovery.dir/wqueue.cpp.o"
   )
 SET(CMAKE_CXX_COMPILER_ID "GNU")
 
